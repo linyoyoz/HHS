1dab86469880c2a8412cfe8aabe60ce36568f33e
linyoyoz <linyoyoz@gmail.com>
2018-12-11 06:46:17 -0500

    Update7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -94,6 +94,9 @@ class UsersController < ApplicationController
   # DELETE /users/1.json
   def destroy
     @user.destroy
+	if @user.name==session['user']#刪除自己
+		session['user']=0
+	end
     respond_to do |format|
       format.html { redirect_to users_url, notice: '刪除成功' }
       format.json { head :no_content }
